247dc324568b9d540d9b385e2831a6ce1e766cbd
Gichure Maina <76261570+KyleGichez@users.noreply.github.com>
2022-01-30 09:48:13 +0300

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Image from './images/image-header-desktop.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app--container">
+        <div className="app--container--card1">
+          <h1 className="main--heading">Get <span className="insights">insights</span> that help your business grow.</h1>
+          <p className="main--paragraph">Discover the benefits of data analytics and make better decisions regarding avenue, customer experience, and overall efficiency. </p>
+            <div className="app--container--stats">
+              <span>10k+ <br/> <p className="stat--headings">Companies</p></span>
+              <span>314 <br/> <p className="stat--headings">Templates</p></span>
+              <span>12M+ <br/> <p className="stat--headings">Queries</p></span>
+            </div>
+        </div>
+        <div className="app--container--card2">
+          <img src={Image} alt="work_place_image"/>
+        </div>
+      </div>
     </div>
   );
 }
